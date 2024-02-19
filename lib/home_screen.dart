d833905b2346e99bc6ba14956150fe9364dce4d9
muhammadnaqeeb <muhammadnaqeebawan@gmail.com>
2024-02-19 13:22:11 +0500

    dialog alert using getx

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,8 +10,43 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("GetX"),
       ),
-      body: const Column(
-        children: [],
+      body: Column(
+        children: [
+          Card(
+            child: ListTile(
+              title: const Text("GetX dialog Alert"),
+              subtitle: const Text("Click On the Card"),
+              onTap: () {
+                Get.defaultDialog(
+                  title: "Hello",
+                  contentPadding: const EdgeInsets.all(10),
+                  middleText:
+                      "Dialog using GetX", // not more then 3 lines, you can yes 'content' for this
+                  // textConfirm: "Yes",
+                  // textCancel: "No",
+                  confirm: TextButton(
+                    onPressed: () {},
+                    child: const Text("Yes"),
+                  ),
+                  cancel: TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text("Cancel"),
+                  ),
+                  // content: const Column(
+                  //   children: [
+                  //     Text("Hi"),
+                  //     Text("Hi"),
+                  //     Text("Hi"),
+                  //     Text("Hi"),
+                  //   ],
+                  // ),
+                );
+              },
+            ),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
